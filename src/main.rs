204e69521538009d6757d740714f5de6bb04e0d7
Ovidiu Ionescu <ovidiu@ionescu.net>
2019-03-25 07:52:19 +0100

    added lifetimes

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use std::collections::HashMap;
     LineEnd
   }
 
+struct HashToHash<'a>(HashMap<&'a str, HashToHash<'a>>);
+
 fn main() {
   let args: Vec<String> = env::args().collect();
   let filename = &args[1];
@@ -20,52 +22,37 @@ fn main() {
 
   let entire_file = unsafe { str::from_utf8_unchecked(&mmap) };
 
+  let mut domain_index = HashToHash(HashMap::new());
 
-  let mut status = LinePosition::LineEnd;
-  let mut start = 0;
-  let mut end = 0;
-  let mut count = 0;
-  let mut line_count = 0;
-  for (i, c) in entire_file.char_indices() {
-    count+=1;
-    match c {
-      '\n' | '\r' => {
-        if let LinePosition::LineStart = status {
-          status = LinePosition::LineEnd;
-          if i > start + 1 {
-            end = i;
-            process_line(&entire_file[start..end]);
-          }
-        }
-      },
-      _ => {
-        if LinePosition::LineEnd == status {
-          status = LinePosition::LineStart;
-          start = i;
-          line_count += 1;
-        }
-      }
-    }
+  for line in entire_file.lines() {
+    // process_line(line);
+    add_domain(&mut domain_index, line);
+
   }
-  if end < start {
-    process_line(&entire_file[start..]);
-  }
-  println!("Stats: characters {}, lines {}", count, line_count);
 
-
-  // println!("From file {}", &tot_fisierul[1..10]);
-  // println!("Hello, world!");
 }
 
 fn process_line(line: &str) {
+  if line.len() < 1 {
+    return
+  }
   for item in line.split('.').rev() {
     print!("{};", item);
   }
   println!(" = {}", line);
 }
 
-struct HashToHash<'a>(HashMap<&'a str, HashToHash<'a>>);
+fn add_domain<'b>(domain_index: &mut HashToHash<'b>, line: &'b str) {
+  if line.len() < 1 {
+    return
+  }
 
-struct DomainIndex<'a> {
-  map: HashToHash<'a>
+  let mut index = domain_index;
+  for item in line.split('.').rev() {
+    print!("{};", item);
+
+    index = index.0.entry(item).or_insert(HashToHash(HashMap::new()));
+
+  }
+  println!(" = {}", line);
 }
